7e58310226e9d8fea7bba36c4b2683284f9ea082
LanAnhNghiem <lananh.nghiem0502@gmail.com>
2018-04-08 22:15:30 +0700

    Fix cart and add correct search

--- BookStore/Controllers/Search1Controller.cs
+++ BookStore/Controllers/Search1Controller.cs
@@ -90,6 +90,14 @@ namespace BookStore.Controllers
 
             if (authorId != null)
             {
+                //Product newPrd = new Product();
+                
+                //var tmp = products.Join(db.ProductDetails, id => id.ID, prId => prId.ProductId, (id, prId) 
+                //    => new { id.ID, prId.AuthorId } as ProductItemView)
+                //    .Where(a => a.prId.AuthorId == authorId).ToList();
+                
+                //products = products.Where(n => tmp.Contains(n.ID));
+
                 List<Product> tmp = products.ToList();
                 List<Product> tmp2 = new List<Product>(tmp);
                 foreach (var singleProduct in tmp2)
@@ -105,7 +113,7 @@ namespace BookStore.Controllers
                     }
                 }
                 if (tmp.Count == 0)
-                    return PartialView("ItemPartial", tmp);
+                    return Json(tmp, JsonRequestBehavior.AllowGet);
 
                 try
                 {
@@ -115,6 +123,7 @@ namespace BookStore.Controllers
                 {
                     return Json("lỗi không xác định", JsonRequestBehavior.AllowGet);
                 }
+
             }
 
             if (categoryId != null)
@@ -141,6 +150,100 @@ namespace BookStore.Controllers
             // trả về list products
             return PartialView("ItemPartial", products.ToList());
            
+        }
+
+        ///
+        /// <summary>
+        /// search data [json]
+        /// </summary>
+        [HttpGet]
+        public ActionResult SearchCorrectly(
+            string key
+            , int? authorId, int? categoryId
+            , int? costBegin, int? costEnd)
+        {// search từ trang search, gọi lệnh search cùng các filter parameter
+
+            if (costBegin != null)
+            {
+                costBegin *= 1000;
+            }
+            if (costEnd != null)
+            {
+                costEnd *= 1000;
+            }
+            IQueryable<Product> products;
+            if (!string.IsNullOrEmpty(key))
+            {
+                products = db.Products.Where(p => p.Name.Contains(key));
+            }
+            else
+            {
+                products = db.Products;
+            }
+
+            if (authorId != null)
+            {
+                List<Product> tmp = products.ToList();
+                List<Product> tmp2 = new List<Product>(tmp);
+                foreach (var singleProduct in tmp2)
+                {
+                    var details = singleProduct.ProductDetails;
+                    if (details != null)
+                    {
+                        var found = details.Where(pd => pd.AuthorId == authorId);
+                        if (found == null || found.Count() == 0)
+                        {
+                            tmp.Remove(singleProduct);
+                        }
+                    }
+                }
+                if (tmp.Count == 0)
+                    return Json(tmp, JsonRequestBehavior.AllowGet);
+
+                try
+                {
+                    products = tmp.AsQueryable();
+                }
+                catch (Exception)
+                {
+                    return Json("lỗi không xác định", JsonRequestBehavior.AllowGet);
+                }
+            }
+
+            if (categoryId != null)
+            {
+                products = products.Where(p => p.CategoryId == categoryId);
+
+                if (products.Count() == 0)
+                    return Json(new List<Product>(), JsonRequestBehavior.AllowGet);
+            }
+
+            if (costBegin != null)
+            {
+                products = products.Where(p => p.Price >= costBegin);
+                if (products.Count() == 0)
+                    return Json(new List<Product>(), JsonRequestBehavior.AllowGet);
+            }
+            if (costEnd != null)
+            {
+                products = products.Where(p => p.Price <= costEnd);
+                if (products.Count() == 0)
+                    return Json(new List<Product>(), JsonRequestBehavior.AllowGet);
+            }
+
+            // trả về json dữ liệu sau khi filter
+            var result = new JsonNetResult
+            {
+                Data = products.ToList(),
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet,
+                Settings = {
+            ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+            MaxDepth = 3// product, productdetail, author
+                }
+            };
+            // trả về list products
+            return PartialView("ItemPartial", products.ToList());
+
         }
     }
 }
---
+++ BookStore/Models/ProductItemView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStore.Models
+{
+    public class ProductItemView
+    {
+        private int id { get; set; }
+        private int prdId { get; set; }
+
+        public ProductItemView(int id, int prdId)
+        {
+            this.id = id;
+            this.prdId = prdId;
+        }
+    }
+}
