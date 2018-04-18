acf5a477606ba9f811ab6c05dc8a01c356237063
Khang Dinh Hoang <taigama@users.noreply.github.com>
2018-04-18 16:27:07 +0700

    side view misc

--- BookStore/Controllers/Home1Controller.cs
+++ BookStore/Controllers/Home1Controller.cs
@@ -17,8 +17,8 @@ namespace BookStore.Controllers
         public ActionResult Index()
         {
             //The side menu data
-            ViewData["Categories"] = db.Categories.Take(5).ToList();
-            ViewData["Authors"] = db.Authors.Take(5).ToList();
+            ViewData["Categories"] = db.Categories.OrderByDescending(cate => cate.NumberOfProducts).Take(5).ToList();
+            ViewData["Authors"] = db.Authors.OrderByDescending(au => au.NumberOfBooks).Take(5).ToList();
 
             // main page data
             ViewData["Products"] = db.Products.ToList();
@@ -49,8 +49,8 @@ namespace BookStore.Controllers
             }
 
             //The side menu data
-            ViewData["Categories"] = db.Categories.ToList();
-            ViewData["Authors"] = db.Authors.ToList();
+            ViewData["Categories"] = db.Categories.OrderByDescending(cate => cate.NumberOfProducts).Take(5).ToList();
+            ViewData["Authors"] = db.Authors.OrderByDescending(au => au.NumberOfBooks).Take(5).ToList();
 
             // main page data
             return View(author);
